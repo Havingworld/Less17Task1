cb2a3499e4eb49ff86e823a97b9284f006dc629d
Владимир Тетерин <bigsinner@yandex.ru>
2022-03-11 07:25:21 +1000

    Добавьте файлы проекта.

---
+++ Less17Task1.cpp
@@ -0,0 +1,22 @@
+﻿/*
+Написать функцию, принимающую два указателя на int и меняет местами содержимое данных указателей.
+*/
+
+#include <iostream>
+using namespace std;
+
+void swap (int* pa, int* pb)
+{
+    int c = *pb;
+    *pb = *pa;
+    *pa = c;
+}
+
+int main()
+{
+    int a = 11, b = 22;
+    swap(&a, &b);
+    cout << a << " " << b;
+}
+
+
